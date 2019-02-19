db4756e6c712c38550774a4ca1d44c4346995256
EconClass <bizmail327@gmail.com>
2019-02-19 12:05:48 -0800

    testing harvard museum api

---
+++ controllers/harvardPics.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const restler = require("restler");
+module.exports = (app) => {
+    // test response
+    app.get('/api/test', (req, res) => {
+        restler.get("https://api.harvardartmuseums.org/object", {
+            query: {
+                apikey: process.env.KEY,
+                title: "dog",
+                fields: "objectnumber,title,dated",
+            }
+        })
+        .on("complete", function(data, response) {
+            res.send(data.records);
+        });
+    });
+
+    // get all periods
+    app.get('/api/periods', (req, res) => {
+        restler.get("https://api.harvardartmuseums.org/period", {
+            query: {
+                apikey: process.env.KEY,
+            }
+        })
+        .on("complete", function(data, response) {
+            res.send(data.records);
+        });
+    });
+
+    // get one image by id
+    app.get('/api/pic/:id', (req, res) => {
+        restler.get(`https://api.harvardartmuseums.org/image/${req.params.id}`, {
+            query: {
+                apikey: process.env.KEY,
+            }
+        })
+        .on("complete", function(data, response) {
+            res.send(data);
+        });
+    });
+};
