0971a86fb02c99584e01d37dfe168b51bd1f3586
EconClass <bizmail327@gmail.com>
2019-02-18 21:05:00 -0800

    switched to a different external api

--- controllers/images.js
+++ controllers/images.js
@@ -4,47 +4,47 @@ const app = express();
 
 
 module.exports = (app) => {
-    let xAppToken;
+    // let xAppToken;
 
-    app.get('/api/image/token', (req, res) => {
-        axios.post('https://api.artsy.net/api/tokens/xapp_token', {
-            client_id: process.env.ID,
-            client_secret: process.env.SECRET
-        })
-        .then(function (response) {
-            xAppToken = response.data.token;
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-        res.send(xAppToken);
-    });
+    // app.get('/api/image/token', (req, res) => {
+    //     axios.post('https://api.artsy.net/api/tokens/xapp_token', {
+    //         client_id: process.env.ID,
+    //         client_secret: process.env.SECRET
+    //     })
+    //     .then(function (response) {
+    //         xAppToken = response.data.token;
+    //     })
+    //     .catch(function (error) {
+    //         console.log(error);
+    //     });
+    //     res.send(xAppToken);
+    // });
 
-    app.get('/api/image/:artist', (req, res) => {
-        axios.get(`https://api.artsy.net/api/artists/${req.params.artist}`, {
-            headers: { 'X-Xapp-Token': process.env.TOKEN}
-        })
-        .then( response => {
-            // const images = response.data._links;
-            res.send(response.data._links)
-        })
-        .catch( error => {
-            console.log(error);
-        });
-        // res.send(images)
-    });
-
-    // app.get('/api/image/artworks', (req, res) => {
-    //     axios.get("https://api.artsy.net/api/artworks/4d8b92b34eb68a1b2c0003f4", {
+    // app.get('/api/image/:artist', (req, res) => {
+    //     axios.get(`https://api.artsy.net/api/artists/${req.params.artist}`, {
     //         headers: { 'X-Xapp-Token': process.env.TOKEN}
     //     })
     //     .then( response => {
-    //         console.log(response)
-    //         // res.send(response.data._links)
+    //         // const images = response.data._links;
+    //         res.send(response.data._links)
     //     })
     //     .catch( error => {
     //         console.log(error);
     //     });
     //     // res.send(images)
     // });
+
+    app.get('/api/image/artworks', (req, res) => {
+        axios.get("https://api.art.rmngp.fr/v1/works", {
+            headers: { "ApiKey": "demo"}
+        })
+        .then( response => {
+            console.log(response)
+            res.send(response.data.hits.hits)
+        })
+        .catch( error => {
+            console.log(error.message);
+        });
+        // res.send(images)
+    });
 };
